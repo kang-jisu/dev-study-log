b35f529dd2ee4a53e8a6f7b696ef16d094c95d92
kang-jisu <rkdwltn0425@gmail.com>
2022-02-05 19:33:02 +0900

    initial commit

---
+++ study-log/src/main/java/com/dev/studylog/StudyLogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.studylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLogApplication.class, args);
+    }
+
+}
---
+++ study-log/src/test/java/com/dev/studylog/StudyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.studylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

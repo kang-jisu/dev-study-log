864a864495d96dbb909e34edd0849d67d47cfe9e
kang-jisu <rkdwltn0425@gmail.com>
2022-02-10 12:07:16 +0900

    Enum 소문자 검사 테스트

---
+++ study-log/src/test/java/com/dev/studylog/enumtest/EnumLowerCaseTest.java
@@ -0,0 +1,37 @@
+package com.dev.studylog.enumtest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.util.EnumUtils;
+
+import java.util.Arrays;
+
+public class EnumLowerCaseTest {
+
+    static private enum GameType{
+        REGULAR,
+        LEAGUE,
+    }
+
+    @Test
+    @DisplayName("소문자 변환 실패")
+    public void enumTest(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> GameType.valueOf("regular"));
+
+    }
+
+    @Test
+    @DisplayName("EnumUtil")
+    public void enumUtil(){
+        // 내부에서 equalsIgnoreCase로 검사 하고있음
+        GameType gameType= EnumUtils.findEnumInsensitiveCase(GameType.class, "regular");
+        System.out.println(gameType);
+
+        boolean Tf = Arrays.stream(GameType.values()).filter(
+                e -> e.name().equalsIgnoreCase("regular")
+        )
+        .findAny().isPresent();
+        System.out.println(String.valueOf(Tf));
+    }
+}
